5e150d965247ba8f0a412caab73fd8245e9e7646
kailua <kailua@project2by2.jp>
2025-08-21 20:52:05 +0900

    Revert "M06: Switch back to QTI perf service"
    This reverts commit a214992a6a911870492486d8591caf87c8558694.

--- device.mk
+++ device.mk
@@ -310,10 +310,13 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/parrot/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+TARGET_PROVIDES_POWERHAL := true
 
 # Properties
 include $(DEVICE_PATH)/configs/properties/default.mk
@@ -404,7 +407,11 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
+    $(DEVICE_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_COPY_FILES += \
