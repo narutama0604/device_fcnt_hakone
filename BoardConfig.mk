7412e193bd64c5f36694df70fbe47449cb43bc44
luk1337 <priv.luk@gmail.com>
2026-05-24 00:27:01 +0900

    M06: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -249,8 +249,7 @@ DEVICE_MANIFEST_PARROT_FILES := \
     $(DEVICE_PATH)/configs/vintf/manifest_parrot.xml \
     hardware/qcom-caf/sm8450/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
 
 # WiFi
